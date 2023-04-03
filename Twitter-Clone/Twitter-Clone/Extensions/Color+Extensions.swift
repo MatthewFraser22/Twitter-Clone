470ac7de12eafdb73e25ab7458d163f19a5cd0cb
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2023-04-03 13:27:44 -0400

    #3 Advanced TabView Implementation
    - Added extension to color to access custom colors easier
    - implemented advanced tabview that changes color to the current tab the user is on

---
+++ Twitter-Clone/Twitter-Clone/Extensions/Color+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extensions.swift
+//  Twitter-Clone
+//
+//  Created by Matthew Fraser on 2023-04-03.
+//
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let backgroundblue = Color(Colors.background.rawValue)
+}
+
+private enum Colors: String {
+    case background = "bg"
+}
--- Twitter-Clone/Twitter-Clone/Views/Main/HomeView.swift
+++ Twitter-Clone/Twitter-Clone/Views/Main/HomeView.swift
@@ -7,27 +7,60 @@
 
 import SwiftUI
 
+fileprivate enum SelectedTabViewIndex: Int {
+    case feed = 0
+    case search = 1
+    case notification = 2
+    case messaege = 3
+}
+
 struct HomeView: View {
+    @State fileprivate var selectedIndex: SelectedTabViewIndex = .feed
+
     var body: some View {
         VStack {
             ZStack {
                 TabView {
                     FeedView()
+                        .onTapGesture {
+                            selectedIndex = .feed
+                        }
                         .tabItem {
                             Image("Home")
+                                .renderingMode(.template)
+                                .foregroundColor(selectedIndex == .feed ? .backgroundblue : .gray)
                         }
+                        .tag(SelectedTabViewIndex.feed.rawValue)
                     SearchView()
+                        .onTapGesture {
+                            selectedIndex = .search
+                        }
                         .tabItem {
                             Image("Search")
+                                .renderingMode(.template)
+                                .foregroundColor(selectedIndex == .search ? .backgroundblue : .gray)
                         }
+                        .tag(SelectedTabViewIndex.search.rawValue)
                     NotificationsView()
+                        .onTapGesture {
+                            selectedIndex = .notification
+                        }
                         .tabItem {
                             Image("Notifications")
+                                .renderingMode(.template)
+                                .foregroundColor(selectedIndex == .notification ? .backgroundblue : .gray)
                         }
+                        .tag(SelectedTabViewIndex.notification.rawValue)
                     MessagesView()
+                        .onTapGesture {
+                            selectedIndex = .messaege
+                        }
                         .tabItem {
                             Image("Messages")
+                                .renderingMode(.template)
+                                .foregroundColor(selectedIndex == .messaege ? .backgroundblue : .gray)
                         }
+                        .tag(SelectedTabViewIndex.messaege.rawValue)
                 }
             }
         }
