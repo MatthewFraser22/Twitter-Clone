4c8ca2a7d36ec3b39f4c6143f4ddac3c3c44faec
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2023-04-05 22:34:27 -0400

    FEATURE - Implemented messages view (#14)
    - created messages cell view for a reusable message cell
    - dummy data in message view until back end gets implemented

--- Twitter-Clone/Twitter-Clone/Views/Messages/MessagesView.swift
+++ Twitter-Clone/Twitter-Clone/Views/Messages/MessagesView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct MessagesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+
+            ScrollView(.vertical, showsIndicators: true) {
+                ForEach(0..<20) { _ in
+                    MessageCellView()
+                }
+            }
+        }
     }
 }
 
