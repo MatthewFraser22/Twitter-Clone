3d825d8097b9081b40f071f4cb522651bbfe5807
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2023-04-08 14:44:48 -0400

    FEATURE - Implemented the profile UI #23

--- Twitter-Clone/Twitter-Clone/Views/Profile/UserProfileView.swift
+++ Twitter-Clone/Twitter-Clone/Views/Profile/UserProfileView.swift
@@ -10,14 +10,18 @@ import SwiftUI
 struct UserProfileView: View {
     @State var offset: CGFloat = 0.0
     @State var titleOffset: CGFloat = 0.0
+    @State var tabBarOffset: CGFloat = 0.0
+    @State var currentTab: ProfileTabButtonOptions = .tweets
+
+    @Namespace var animation
 
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack {
                 GeometryReader { (geometryProxy) -> AnyView in
-
+                    
                     let minY = geometryProxy.frame(in: .global).minY
-
+                    
                     DispatchQueue.main.async {
                         self.offset = minY
                     }
@@ -32,7 +36,8 @@ struct UserProfileView: View {
                                     height: minY > 0 ? 180 + minY : 180,
                                     alignment: .center
                                 )
-
+                                .cornerRadius(0)
+                            
                             BlurViewRepresentable()
                                 .opacity(blurViewOpacity())
 
@@ -42,14 +47,18 @@ struct UserProfileView: View {
                             .frame(height: minY > 0 ? 180 + minY : nil)
                             .offset(y: minY > 0 ? -minY : -minY < 80 ? 0 : -minY - 80)
                     )
-                    
+
                 }
+                .frame(height: 180)
+                .zIndex(1)
+
+                profileBioView
 
             }
         }
         .ignoresSafeArea(.all)
     }
-    
+
     private var nameDetailsView: some View {
         VStack(alignment: .center, spacing: 5) {
             Text("Matt")
@@ -61,6 +70,131 @@ struct UserProfileView: View {
         .offset(y: 120) // Push the text down by offseting 120 px in y direction
         .offset(y: titleOffset > 100 ? 0 : -getTitleOffset())
         .opacity(titleOffset < 100 ? 1 : 0)
+    }
+
+    private var profileBioView: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image("blankpp")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipShape(Circle())
+                    .frame(width: 60, height: 60)
+                    .padding(8)
+                    .background(Color.white)
+                    .clipShape(Circle())
+                    .offset(y: offset < 0 ? getOffset() - 20 : -20)
+                    .scaleEffect(getScale())
+
+                Spacer(minLength: 0)
+
+                Button {
+                    
+                } label: {
+                    Text("Edit Profile")
+                        .foregroundColor(.blue)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .background(
+                            Capsule()
+                                .stroke(Color.blue, lineWidth: 1.5)
+                        )
+                }
+
+            }
+            .padding(.top, -25) // get overlay on ZStack
+            .padding(.bottom, -10)
+
+            profileBio
+            profileTabButtons
+            feedView
+        }
+        .padding()
+        .zIndex(-offset > 80 ? 0 : 1)
+    }
+    
+    private var profileBio: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Matt")
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+
+            Text("@matt_fraser")
+                .foregroundColor(.gray)
+
+            Text("This is my profile bio")
+            
+            HStack(spacing: 5) {
+                Text("1")
+                    .foregroundColor(.primary)
+                    .fontWeight(.semibold)
+                
+                Text("Followers")
+                    .foregroundColor(.gray)
+                
+                Text("5")
+                    .foregroundColor(.primary)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 10)
+                
+                Text("Following")
+                    .foregroundColor(.gray)
+            }
+        }
+        .overlay(
+            GeometryReader { proxy -> Color in
+                let minY = proxy.frame(in: .global).minY
+                
+                DispatchQueue.main.async {
+                    self.titleOffset = minY
+                }
+                
+                return Color.clear
+            }.frame(width: 0, height: 0)
+            , alignment: .top
+        )
+        .padding()
+    }
+
+    private var profileTabButtons: some View {
+        VStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 0) {
+                    ForEach(ProfileTabButtonOptions.allCases, id: \.rawValue) { tabButton in
+                        ProfileTabButtonView(currentTab: $currentTab, title: tabButton, animation: animation)
+                    }
+                }
+            }
+        }
+        .padding(.top, 30)
+        .background(Color.white)
+        .offset(y: tabBarOffset < 90 ? -tabBarOffset + 90 : 0)
+        .overlay(
+            GeometryReader { proxy -> Color in
+                let minY = proxy.frame(in: .global).minY
+                
+                DispatchQueue.main.async {
+                    self.tabBarOffset = minY
+                }
+                
+                return Color.clear
+            }.frame(width: 0, height: 0)
+            , alignment: .top
+        ).zIndex(1)
+    }
+
+    private var feedView: some View {
+        VStack(spacing: 18) {
+            TweetCellView(tweet: "HI", tweetImage: "post")
+            Divider()
+            ForEach(0..<20, id: \.self) { _ in
+                TweetCellView(tweet: "sdsdfrgerger")
+                Divider()
+            }
+        }
+        .padding(.top)
+        .zIndex(0)
     }
 
     //
@@ -99,7 +233,7 @@ struct UserProfileView_Previews: PreviewProvider {
     }
 }
 
-public enum ProfileTabButtonOptions: String {
+public enum ProfileTabButtonOptions: String, CaseIterable {
     case tweets = "Tweets"
     case tweetsAndLikes = "Tweets & Likes"
     case media = "Media"
