874f3b35910e7831f61e0b7bbc5023332936d379
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2023-04-12 17:21:02 -0400

    ENDPOINT - upload tweet image

--- twitter-api/src/routers/tweet.js
+++ twitter-api/src/routers/tweet.js
@@ -1,11 +1,40 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const Tweet = require('../models/tweet')
 
 const router = new express.Router()
 
 const auth = require('../middleware/auth')
 
+// HELPER FUNCTIONS
+
+const upload = multer({
+    limits: {
+        fileSize: 100000000
+    }
+})
+
 // ENDPOINTS
+
+// UPLOAD IMAGE TO TWEET ROUTE
+router.post('/uploadTweetImage/:id', auth, upload.single('upload'), async (req, res) => {
+    const tweet = await Tweet.findOne({ _id: req.params.id })
+    console.log(tweet)
+
+    if (!tweet) {
+        throw new Error('Cannot find the tweet')
+    }
+
+    const buffer = await sharp(req.file.buffer).resize( { width: 350, height: 350 } ).png().toBuffer()
+
+    tweet.image = buffer
+    await tweet.save()
+    
+    res.send()
+}, (error, req, res, next) => { // can do this because we are using middle ware
+    res.status(400).send( { error: error.message } )
+})
 
 // POST TWEET
 router.post('/tweets', auth, async (req, res) => {
